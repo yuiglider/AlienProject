45df03dbc8c0bceffaff7e8a6c3256620b433e7e
gongyuming <[email]>
2017-07-04 13:38:57 +0800

    修改目录结构

--- app/src/main/java/com/hc/alienproject/MainActivity.java
+++ app/src/main/java/com/hc/alienproject/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.yui.alienproject;
+package com.hc.alienproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.yui.alienproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
