0c575dab554479ebe9e47c0309f1f04fc665bc1a
gongyuming <[email]>
2017-07-07 16:54:26 +0800

    添加项目依赖

--- app/src/main/java/com/hc/alienproject/MainActivity.java
+++ app/src/main/java/com/hc/alienproject/MainActivity.java
@@ -2,7 +2,6 @@ package com.hc.alienproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.example.yui.alienproject.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
