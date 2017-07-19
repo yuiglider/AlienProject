44ce1e5472c32a8a468169bba3501970e6e0a59a
gongyuming <[email]>
2017-07-19 15:52:19 +0800

    添加 Base 和依赖

---
+++ app/src/main/java/com/hc/alienproject/base/BaseActivity.java
@@ -0,0 +1,40 @@
+package com.hc.alienproject.base;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by Administrator on 2017/7/19.
+ */
+
+public abstract class BaseActivity extends RxAppCompatActivity{
+      private Unbinder binder;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutID());
+        binder= ButterKnife.bind(this);
+        initViews(savedInstanceState);
+        initToolBar();
+    }
+
+    protected abstract void initToolBar();
+
+    protected abstract void initViews(Bundle savedInstanceState);
+
+
+    public abstract int getLayoutID();
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binder.unbind();
+    }
+}
---
+++ app/src/main/java/com/hc/alienproject/base/BaseFragment.java
@@ -0,0 +1,44 @@
+package com.hc.alienproject.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.trello.rxlifecycle2.components.support.RxFragment;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by Administrator on 2017/7/19.
+ */
+
+public abstract class BaseFragment extends RxFragment{
+    private Unbinder binder;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(getLayoutID(), container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binder= ButterKnife.bind(this,view);
+        CreateView(savedInstanceState);
+    }
+
+    public abstract void CreateView(Bundle savedInstanceState);
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binder.unbind();
+    }
+
+    public abstract LayoutInflater getLayoutID();
+
+}
