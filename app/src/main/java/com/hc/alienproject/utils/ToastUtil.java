4720c8243583effc24e19ed7662245f98910bd10
gongyuming <[email]>
2017-07-21 17:50:08 +0800

    添加全局配置文件

--- app/src/main/java/com/hc/alienproject/utils/ToastUtil.java
+++ app/src/main/java/com/hc/alienproject/utils/ToastUtil.java
@@ -1,5 +1,7 @@
 package com.hc.alienproject.utils;
 
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.widget.Toast;
 
 import com.hc.alienproject.App;
@@ -12,5 +14,9 @@ public class ToastUtil {
 
     public static void handle(String text){
         Toast.makeText(App.getContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    public static void handleView(View view,String text,String action,View.OnClickListener listener){
+        Snackbar.make(view,text,Snackbar.LENGTH_SHORT).setAction(action,listener).show();
     }
 }
